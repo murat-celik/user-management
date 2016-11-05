89d681d7450ce5d83a69c9ec671e7c3e384a733b
murat-celik <muratcelik.27@outlook.com>
2016-11-06 01:26:01 +0300

    Dashboard

--- user/views/default/index.php
+++ user/views/default/index.php
@@ -1,3 +1,29 @@
 <div class="user-default-index">
-    <!--<h1><?php // $this->context->action->uniqueId ?></h1>-->
+    <!--<h1><?php // $this->context->action->uniqueId         ?></h1>-->
+    <div class="tile-group double" style="left: 0px;">
+        <h4 > <span class="text-bold text-primary"> User Managment</span></h4>
+        <div class="tile-container">
+            <div class="tile bg-darkBlue fg-white" data-role="tile" onclick="document.location.href = 'http://gmail.com'" style="opacity: 1; transform: scale(1); transition: 0.3s;">
+                <div class="tile-content iconic">
+                    <span class="icon mif-users"></span>
+                    <span class="tile-badge bg-darkRed">5</span>
+                </div>
+                <span class="tile-label">Users</span>
+            </div>
+            <a href="http://calendar.google.com" class="tile bg-green fg-white" data-role="tile" style="opacity: 1; transform: scale(1); transition: 0.3s;">
+                <div class="tile-content iconic">
+                    <span class="icon mif-user-plus"></span>
+                </div>
+                <span class="tile-label">Create User</span>
+            </a>
+            <div class="tile-large bg-steel fg-white" data-role="tile" data-on-click="document.location.href='http://forecast.io'" style="opacity: 1; transform: scale(1); transition: 0.3s;">
+                <div class="tile-content" id="weather_bg" style="background: top left no-repeat; background-size: cover">
+                    <div class="padding10">
+                        This is Summary Of Users
+                    </div>
+                </div>
+                <span class="tile-label">Summary</span>
+            </div>
+        </div>
+    </div>
 </div>
