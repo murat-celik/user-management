79610b9b3f810c66d412fb091d437159510e2c91
murat-celik <muratcelik.27@outlook.com>
2016-11-07 01:20:10 +0300

    Rbac table script

--- user/migrations/USER_RBAC.sql
+++ user/migrations/USER_RBAC.sql
@@ -1,4 +1,12 @@
-CREATE TABLE IF NOT EXISTS `user` (
+drop table if exists `user`;
+drop table if exists `auth_assignment`;
+drop table if exists `auth_item_child`;
+drop table if exists `auth_item`;
+drop table if exists `auth_rule`;
+
+
+CREATE TABLE IF NOT EXISTS `user` 
+(
   `id` int          NOT NULL AUTO_INCREMENT PRIMARY KEY NOT NULL,
   `firstname`       varchar(128) NOT NULL,
   `lastname`        varchar(128) NOT NULL,
@@ -13,4 +21,45 @@ CREATE TABLE IF NOT EXISTS `user` (
   `datetime_create`  datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `datetime_update`  datetime
 );
+
+create table `auth_rule`
+(
+   `name`                 varchar(64) not null,
+   `data`                 text,
+   `created_at`           integer,
+   `updated_at`           integer,
+    primary key (`name`)
+) engine InnoDB;
+
+create table `auth_item`
+(
+   `name`                 varchar(64) not null,
+   `type`                 integer not null,
+   `description`          text,
+   `rule_name`            varchar(64),
+   `data`                 text,
+   `created_at`           integer,
+   `updated_at`           integer,
+   primary key (`name`),
+   foreign key (`rule_name`) references `auth_rule` (`name`) on delete set null on update cascade,
+   key `type` (`type`)
+) engine InnoDB;
+
+create table `auth_item_child`
+(
+   `parent`               varchar(64) not null,
+   `child`                varchar(64) not null,
+   primary key (`parent`, `child`),
+   foreign key (`parent`) references `auth_item` (`name`) on delete cascade on update cascade,
+   foreign key (`child`)  references `auth_item` (`name`) on delete cascade on update cascade
+) engine InnoDB;
+
+create table `auth_assignment`
+(
+   `item_name`            varchar(64) not null,
+   `user_id`              varchar(64) not null,
+   `created_at`           integer,
+   primary key (`item_name`, `user_id`),
+   foreign key (`item_name`) references `auth_item` (`name`) on delete cascade on update cascade
+) engine InnoDB;
 
